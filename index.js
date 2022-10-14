90dbf02b381e5711aa36a4544a2cfa493522ab55
zettacampmuhammadnurrochim <muhammad.rochim@zettacamp.pro>
2022-10-15 06:22:31 +0700

    jadi

--- index.js
+++ index.js
@@ -3,41 +3,64 @@ const app = exp()
 const jwt = require('jsonwebtoken')
 app.use(exp.json())
 
-const mongoose = require('mongoose')
+let dataBooks = require('./data.json')
+
+const {mongoose, Schema} = require('mongoose')
 
 const db = "library";
-const port = 5000;
+const port = 8000;
 
-mongoose.connect(`mongodb://localhost:27017/${db}`);
+mongoose.connect(`mongodb://127.0.0.0:27017/${db}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=> {
+    console.log('connected');
+}).catch(e=>{
+    console.log(e.message);
+});
 
 
-const BookSchema = {
-    _id: String,
+let BookSchema = new Schema ({
     image: String,
     title: String,
     author: String,
     price: String,
     original_url: String,
     url: String,
-}
+    slug : String
+})
 
-const collectionBooks = mongoose.model('collectionBooks', BookSchema);
+let collectionBooks = mongoose.model('buku', BookSchema);
+const key = "rochim"
+
+app.get('/show', async (req, res) => {
+    const {authorization} = req.headers
+
+    const decode = await jwt.decode(authorization.replace('Bearer ', ''), key)
 
 
-app.get('/show', (req, res) => {
-    collectionBooks.find({}, function (err, bookList) {
-        res.send({
-            databuku: bookList
-        });
-    });
+    if (authorization !== null) {
+        if (decode !== null) {
+            
+            // let resData = await collectionBooks.find({});
+            // console.log(resData);
+            res.json(dataBooks);
+        
+            // res.send({'data_decode' :  resData})
+        }else{
+            res.send('token is invalid')
+        }
+    }else{
+        res.send('authorization is null')        
+    }
 })
 
 app.post('/login', (req, res) => {
     let {email,password} = req.body;
     
     var token = jwt.sign({
-        foo: 'bar'
-    }, email);
+        "email": email
+    }, key)
 
     if (email == "muhammad.0206@students.amikom.ac.id" && password == "12345678") {
         res.send({
@@ -49,5 +72,12 @@ app.post('/login', (req, res) => {
         res.send("you are not authorized to access this page");
     }
 });
+
+app.use('/', (req, res)=>{
+    res.status(404).send({
+        status : 404,
+        description : "route not found"
+    });
+})
 
 app.listen(port)
